89fff46c2c540bac967c0aba4894677be590d7d6
Manami Ueda <manamiueda1@gmail.com>
2021-02-10 16:48:02 -0500

    added list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const title = "React";
-
-const welcome = {
-  greeting: "Hey",
-  title: "React",
-};
-
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>hello {getTitle("React")}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search" type="text">
         Search:
       </label>
+      <hr />
+      {list.map((item) => (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      ))}
       <input id="search" type="text" />
     </div>
   );
