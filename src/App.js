adda1b268ef1562e932dbd166806e1ce98504d7e
Manami Ueda <manamiueda1@gmail.com>
2021-02-18 16:42:30 -0500

    edited list

--- src/App.js
+++ src/App.js
@@ -20,51 +20,6 @@ const list = [
   },
 ];
 
-let studentData = [
-  {
-    id: 1,
-    name: "Eddy",
-    color: "yellow",
-    school: "Nebula Academy",
-  },
-  {
-    id: 2,
-    name: "Chelsea",
-    color: "purple",
-    school: "Nebula Academy",
-  },
-  {
-    id: 3,
-    name: "Tauseef",
-    color: "orange",
-    school: "Nebula Academy",
-  },
-  {
-    id: 4,
-    name: "Maurice",
-    color: "red",
-    school: "Nebula Academy",
-  },
-  {
-    id: 5,
-    name: "D'aja",
-    color: "blue",
-    school: "Nebula Academy",
-  },
-  {
-    id: 6,
-    name: "Jasmine",
-    color: "pink",
-    school: "Nebula Academy",
-  },
-  {
-    id: 7,
-    name: "Anitria",
-    color: "green",
-    school: "Nebula Academy",
-  },
-];
-
 function App() {
   return (
     <div>
