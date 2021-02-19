7e699b619502827ac17dc4462cd335dfca53b50c
Manami Ueda <manamiueda1@gmail.com>
2021-02-19 15:04:11 -0500

    up until React Hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -30,14 +34,15 @@ function App() {
       </label>
       <hr />
 
-      <input id="search" type="text" />
-      <List />
+      <input id="search" type="text" onChange={handleChange} />
+      <List list={stories} />
     </div>
   );
 }
 
-function List() {
-  return list.map((item) => (
+const List = (props) => {
+  console.log(props);
+  return props.list.map((item) => (
     <div className="card" key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
@@ -47,6 +52,6 @@ function List() {
       <span>{item.points}</span>
     </div>
   ));
-}
+};
 
 export default App;
