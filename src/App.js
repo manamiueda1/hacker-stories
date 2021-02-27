fde6e128b250912f81d87827e40ee96215fb7aa9
Manami Ueda <manamiueda1@gmail.com>
2021-02-26 19:35:34 -0500

    passed down search funciton

--- src/App.js
+++ src/App.js
@@ -21,34 +21,42 @@ function App() {
       objectID: 1,
     },
   ];
+  const [searchTerm, setSearchTerm] = React.useState("React");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) => {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search />
+      <Search search={searchTerm} onSearch={handleSearch} />
       <hr />
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 }
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = React.useState("");
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
+const Search = (props) => {
   return (
     <div>
       <label htmlFor="search" type="text">
         Search:
       </label>
 
-      <input id="search" type="text" onChange={handleChange} />
+      <input
+        id="search"
+        type="text"
+        value={props.search}
+        onChange={props.onSearch}
+      />
 
       <p>
-        Searching for <strong>{searchTerm}</strong>
+        Searching for <strong>{props.search}</strong>
       </p>
     </div>
   );
